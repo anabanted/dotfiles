3750b9cdabe721d7749942e39d574d27c2cf4285
Shunsuke Ono <shunsukeono@MacBook-Air-GSST-50554.local>
2024-07-07 22:42:50 +0900

    divide init and update

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 mkdir ~/.config
 apt-get update
 apt-get install neovim fish
-git submodule update --init
+git submodule init
+git submodule update
 cp -r ./anabanted_astronvim ~/.config/nvim
 cp -r ./fish ~/.config/fish
 cd ~
