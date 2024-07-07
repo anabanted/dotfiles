c58649145731c9622228c536fa52d03d4fe027db
Shunsuke Ono <shunsukeono@MacBook-Air-GSST-50554.local>
2024-07-07 22:39:41 +0900

    add --init

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir ~/.config
 apt-get update
 apt-get install neovim fish
-git submodule update
+git submodule update --init
 cp -r ./anabanted_astronvim ~/.config/nvim
 cp -r ./fish ~/.config/fish
