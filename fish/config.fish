a34d9651bca97c97f6444e4dddae4f72da5a59f5
Shunsuke Ono <shunsukeono@MacBook-Air-GSST-50554.local>
2024-07-07 21:18:45 +0900

    add config of fish and neovim

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set PATH /opt/homebrew/bin $PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
---
+++ link.sh
@@ -0,0 +1,2 @@
+ln -sf ./fish ~/.config/fish
+ln -sf ./anabanted_astronvim ~/.config/nvim
