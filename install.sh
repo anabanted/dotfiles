a1da038206e3d18d0c70d5f898880795e207b3d1
Shunsuke Ono <shunsukeono@MacBook-Air-GSST-50554.local>
2024-07-07 22:37:47 +0900

    update submodule

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir ~/.config
 apt-get update
 apt-get install neovim fish
-git submodule update --init --recursive
+git submodule update
 cp -r ./anabanted_astronvim ~/.config/nvim
 cp -r ./fish ~/.config/fish
